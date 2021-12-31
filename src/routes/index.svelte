13e9450336fe5a64cb4c8cfd400c84da41335f25
Alex <alexandre.olemans@gmail.com>
2021-12-31 03:47:18 +0100

    fix: width too large & feat: posts

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,18 +2,28 @@
 	import Post from './components/Post.svelte';
 	import WelcomeBack from './components/WelcomeBack.svelte';
 
-	let data = {
-		avatar: null,
-		name: null,
-		username: null,
-		content: null
-	};
+	let data = [
+		{
+			avatar: null,
+			name: null,
+			username: null,
+			content: null,
+			createdAt: null,
+			media: {
+				hasMedia: false,
+				type: null,
+				url: null
+			}
+		}
+	];
 </script>
 
-<main class="absolute w-screen top-[9rem] lg:top-[5rem]">
+<main class="absolute w-full top-[9rem] lg:top-[5rem]">
 	<WelcomeBack />
-	<h1 class="text-2xl text-purple md:text-red">Hello World!</h1>
+
 	<div class="relative left-[50%] translate-x-[-50%] max-w-[95%] lg:max-w-[30%]">
-		<Post type="large" {...data} />
+		{#each data as data}
+			<Post type="large" {...data} />
+		{/each}
 	</div>
 </main>
