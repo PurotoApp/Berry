00f854b08348a7a4215d0839d130be53ee4e8c16
Alex <alexandre.olemans@gmail.com>
2022-01-16 02:56:30 +0100

    feat: loading screen

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,11 @@
 <script>
+	import { onMount } from 'svelte';
 	import '../global.css';
 	import Header from '../components/Header.svelte';
+	import Icon from '../components/Icon.svelte';
+
+	let loading = true;
+	onMount(() => loading = false);
 </script>
 
 <main class="absolute min-w-max w-full top-20">
@@ -8,3 +13,11 @@
 </main>
 
 <Header />
+
+{#if loading}
+	<div class="absolute h-full w-full bg-background">
+		<div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 animate-pulse">
+			<Icon size="6rem" stroke="#5351D2"/>
+		</div>
+	</div>
+{/if}
