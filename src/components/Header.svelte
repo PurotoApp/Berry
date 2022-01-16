92a621e050021ba55ee669f7f044cb247f1893af
Alex <alexandre.olemans@gmail.com>
2022-01-16 01:30:30 +0100

    feat: header for small screen

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -20,22 +20,22 @@
 	<div
 		class="absolute w-full min-h-full bg-gray-1 md:bg-background py-12 translate-y-[-15%] md:-translate-y-1/2"
 	/>
-	<div class="relative flex flex-nowrap justify-between w-10/12 mx-auto">
+	<div class="relative flex flex-nowrap mb-2 justify-between w-10/12 mx-auto md:mb-0">
 		<!--    Puroto Icon     -->
-		<div
+		<a
 			class="focus:outline-1 focus:border-white w-8 h-8 cursor-pointer"
-			onclick="window.location.href='/'"
+			href="/"
 		>
 			<Icon />
-		</div>
+		</a>
 
 		<!--    Search Bar     -->
-		<div class="mb-hidden">
+		<div class="md-hidden">
 			<SearchBar />
 		</div>
 
 		<!--	Right Part    -->
-		<div class="w-20 h-8 -ml-12 flex justify-between">
+		<div class="md-hidden w-20 h-8 md:-ml-12 flex justify-between flex-row">
 			<!--	Notification Icon	-->
 			<a href="notification" class="cursor-pointer">
 				<svg
@@ -62,7 +62,7 @@
 				{/if}
 			</a>
 
-			<div class="h-8 w-8 bg-gray-5 rounded-full grabcursor-pointer" on:click={toggleUser}>
+			<div class="h-8 w-8 bg-gray-5 rounded-full cursor-pointer" on:click={toggleUser}>
 				{#if user.isLogOn}
 					<img draggable="false" src={user.avatar} class="h-8 w-8 rounded-full" alt={user.name} />
 					{#if user.hasUnreadMessage}
@@ -111,5 +111,38 @@
 				</div>
 			</div>
 		</div>
+
+		<a href="/user/{user.username}" class="h-8 w-8 bg-gray-5 rounded-full desk-hidden">
+			{#if user.isLogOn}
+				<img draggable="false" src={user.avatar} class="h-8 w-8 rounded-full" alt={user.name} />
+			{/if}
+		</a>
+
+		{#if user.isLogOn}
+			<a href="notification" class="desk-hidden cursor-pointer">
+				<svg
+					width="32"
+					height="32"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+					/>
+				</svg>
+				{#if user.hasUnreadNotification}
+					<div
+						class="text-xs h-3 w-3 rounded-full bg-red-2 -translate-y-3 translate-x--6 pointer-events-none"
+					>
+						<div class="text-xs h-full w-full rounded-full bg-red-1 animate-ping" />
+					</div>
+				{/if}
+			</a>
+		{/if}
 	</div>
 </header>
