ac37fa6d7e783eb026f2321a6b55d7c382cb4f96
Jason Diaz Jimenez <developervulpo@outlook.com>
2022-01-28 19:44:21 -0600

    Moved Comment & Like button

--- src/components/Post.svelte
+++ src/components/Post.svelte
@@ -53,23 +53,6 @@
 		</div>
 		<!-- Buttons -->
 		<div class="flex justify-end items-center mb-1">
-			<!--	Like		-->
-			<div class="w-6 h-6 mx-2">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					class="hover:stroke-red-2 transition"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke="currentColor"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						stroke-width="2"
-						d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-					/>
-				</svg>
-			</div>
 			<!--	Comment		-->
 			<div class="w-6 h-6 mx-2">
 				<svg
@@ -84,6 +67,23 @@
 						stroke-linejoin="round"
 						stroke-width="2"
 						d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+					/>
+				</svg>
+			</div>
+			<!--	Like		-->
+			<div class="w-6 h-6 mx-2">
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					class="hover:stroke-red-2 transition"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
 					/>
 				</svg>
 			</div>
