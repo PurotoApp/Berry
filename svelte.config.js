46dacd95efae84461a6fdfab11499f0381cbf584
Alex <alexandre.olemans@gmail.com>
2022-01-31 23:26:31 +0100

    deps: add svelte-preprocess

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-node';
+import preprocess from 'svelte-preprocess';
 
 export default {
+	preprocess: preprocess(),
 	kit: {
 		adapter: adapter({
 			out: 'build'
