6d5331c38185650811c647d62728b1fddb25c9a2
Alex <alexandre.olemans@gmail.com>
2022-02-11 14:39:13 +0100

    apply format

--- src/components/WelcomeBack.svelte
+++ src/components/WelcomeBack.svelte
@@ -1,3 +1,3 @@
-<div class="relative mx-auto mt-0 z-[12] w-10/12 text-center sm:my-5  md:mt-24 ">
+<div class="relative z-[12] mx-auto mt-0 w-10/12 text-center sm:my-5  md:mt-24 ">
 	<h1 class="text-4xl">Welcome back.</h1>
 </div>
