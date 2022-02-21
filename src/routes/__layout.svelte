c349f1b7755cdc46190a0c67d4605a6110cbb58c
Alex <alexandre.olemans@gmail.com>
2022-02-21 20:54:29 +0100

    fix loading screen

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -9,17 +9,17 @@
 </script>
 
 {#if loading}
-	<div class="absolute h-full w-full bg-background">
-		<div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 animate-pulse">
-			<Icon size="6rem" stroke="#5351D2" />
-		</div>
+<div class="absolute h-full w-full bg-background z-[100]">
+	<div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 animate-pulse">
+		<Icon size="6rem" stroke="#5351D2" />
 	</div>
-{/if}
-
-{#if !loading}
-	<main class="mb:overflow-auto absolute h-[calc(100vh-3.1rem)] w-full min-w-max md:h-full">
-		<slot />
-	</main>
-
-	<Header />
+</div>
+<div class="hidden z-[-1]">
+	<slot />
+</div>
+{:else}
+<Header />
+<main class="mb:overflow-auto absolute h-[calc(100vh-3.1rem)] w-full min-w-max md:h-full">
+	<slot />
+</main>
 {/if}
