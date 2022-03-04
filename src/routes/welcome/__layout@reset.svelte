a940e6ab5b8a98a81ac87b589283de3882ef5d68
Alex <alexandre.olemans@gmail.com>
2022-03-04 23:34:54 +0100

    Welcome page

--- src/routes/welcome/__layout@reset.svelte
+++ src/routes/welcome/__layout@reset.svelte
@@ -1,1 +1,5 @@
+<script lang="ts">
+	import '../../global.css';
+</script>
+
 <slot />
--- src/routes/welcome/index.svelte
+++ src/routes/welcome/index.svelte
@@ -1,3 +1,79 @@
+<script lang="ts">
+	import Icon from '../../components/Icon.svelte';
+</script>
+
 <svelte:head>
 	<title>Puroto - Welcome</title>
 </svelte:head>
+
+<article class="flex h-full justify-center p-12">
+	<div class="flex w-10/12 items-center justify-between">
+		<div class="w-1/2">
+			<h1 class="my-4 text-6xl font-bold">Join a new growing community!</h1>
+			<p>The next generation platform for furries and creators (sample text)</p>
+		</div>
+		<Icon size="6rem" stroke="#6D6BED" />
+	</div>
+</article>
+
+<article class="flex h-full justify-center bg-backgroundDark p-12">
+	<div class="flex w-10/12 items-center justify-between">
+		<div class="w-1/2">
+			<h1 class="my-4 text-4xl font-bold">long description!</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis lorem nulla, ut
+				vulputate purus varius eget. Vivamus non lorem id arcu mattis condimentum. Curabitur
+				elementum vel tortor bibendum tincidunt. Curabitur vehicula eu tellus ut gravida. Donec a
+				metus suscipit, porttitor ex quis, pellentesque mi. Interdum et malesuada fames ac ante
+				ipsum primis in faucibus. Pellentesque volutpat urna nec nunc sodales, nec tincidunt dolor
+				tempor. Vivamus ipsum dui, suscipit quis risus sit amet, tincidunt auctor leo. Pellentesque
+				et est a tortor laoreet pharetra. Pellentesque a odio in nulla dictum tincidunt at id urna.
+				Duis pharetra leo massa, vitae vehicula dui posuere sit amet. Fusce ut fermentum libero, sed
+				tincidunt metus. Nulla sed arcu vel magna rutrum lacinia vitae ac nulla. Nunc sodales
+				efficitur ex, non pulvinar tellus euismod sit amet. Quisque ligula dui, pulvinar nec finibus
+				eu, scelerisque eget nisl. Quisque a eleifend ipsum.
+			</p>
+		</div>
+		<Icon size="6rem" stroke="#6D6BED" />
+	</div>
+</article>
+
+<article class="flex h-full justify-center p-12">
+	<div class="flex w-10/12 items-center justify-between">
+		<div class="w-1/2">
+			<h1 class="my-4 text-4xl font-bold">What makes us different</h1>
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis lorem nulla, ut
+				vulputate purus varius eget. Vivamus non lorem id arcu mattis condimentum. Curabitur
+				elementum vel tortor bibendum tincidunt. Curabitur vehicula eu tellus ut gravida. Donec a
+				metus suscipit, porttitor ex quis, pellentesque mi. Interdum et malesuada fames ac ante
+				ipsum primis in faucibus. Pellentesque volutpat urna nec nunc sodales, nec tincidunt dolor
+				tempor. Vivamus ipsum dui, suscipit quis risus sit amet, tincidunt auctor leo. Pellentesque
+				et est a tortor laoreet pharetra. Pellentesque a odio in nulla dictum tincidunt at id urna.
+				Duis pharetra leo massa, vitae vehicula dui posuere sit amet. Fusce ut fermentum libero, sed
+				tincidunt metus. Nulla sed arcu vel magna rutrum lacinia vitae ac nulla. Nunc sodales
+				efficitur ex, non pulvinar tellus euismod sit amet. Quisque ligula dui, pulvinar nec finibus
+				eu, scelerisque eget nisl. Quisque a eleifend ipsum.
+			</p>
+		</div>
+		<Icon size="6rem" stroke="#6D6BED" />
+	</div>
+</article>
+
+<footer class="flex items-center justify-center bg-backgroundDark p-12">
+	<div class="w-5/12 text-center">
+		<div>
+			<h1 class="my-4 text-4xl font-bold">What are you waiting for?</h1>
+			<p class="my-4">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut venenatis lorem nulla, ut
+				vulputate purus varius eget.
+			</p>
+			<a
+				href="/join"
+				class="relative top-8 select-none rounded-lg bg-blue-4 px-6 py-2 text-xl font-semibold transition hover:bg-blue-4"
+				>Create an account</a
+			>
+		</div>
+	</div>
+</footer>
+<footer class="bg-backgroundDark pt-12">cool links here:</footer>
